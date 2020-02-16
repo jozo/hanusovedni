531b41fd3689a1419dd3b852cd8e335d7354e42d
jozo <hi@jozo.io>
2020-02-16 21:47:25 +0100

    #10 - Fix config for production

--- hanusovedni/settings/production.py
+++ hanusovedni/settings/production.py
@@ -2,7 +2,13 @@ from .base import *
 
 DEBUG = False
 
-try:
-    from .local import *
-except ImportError:
-    pass
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = os.environ["DJANGO_ALLOWED_HOSTS"].split(",")
+
+BASE_URL = os.environ["DJANGO_BASE_URL"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+DATABASES["default"]["PASSWORD"] = os.environ["POSTGRES_PASSWORD"]
