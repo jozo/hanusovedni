215ecec8fb57cddd8873a50b4db3c6bc41b2aca3
jozo <hi@jozo.io>
2020-03-25 14:24:39 +0100

    #54 - Fix permissions on dirs

--- create-server.sh
+++ create-server.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 # Directories for volumes
-mkdir -p "/var/www/static"
-mkdir -p "/var/www/media"
-mkdir -p "/var/www/pgdata"
+mkdir -p -m 0777 "/var/www/static"
+mkdir -p -m 0777  "/var/www/media"
+mkdir -p -m 0777  "/var/www/pgdata"
 
 # Generate private key
 ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
