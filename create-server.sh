428ef12336570d4264761c504ffef8cbb7c9b701
jozo <hi@jozo.io>
2020-03-25 09:06:29 +0100

    #54 - Add option to deploy to production

--- create-server.sh
+++ create-server.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Directories for volumes
+mkdir -p "/var/www/static"
+mkdir -p "/var/www/media"
+mkdir -p "/var/www/pgdata"
+
+# Generate private key
+ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
+
+docker swarm init
 docker network create --driver overlay --scope swarm --attachable hanus-net
