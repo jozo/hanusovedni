695ee1a404a4d412c5f34629d6cda1a679f9898b
jozo <hi@jozo.io>
2020-04-17 21:28:17 +0200

    #51 - Add constant amount from bank

--- src/home/static/js/home/crowdfunding.js
+++ src/home/static/js/home/crowdfunding.js
@@ -2,8 +2,9 @@ $(document).ready(function () {
     $.ajax('https://old.darujme.sk/sk/campaign/feed/2393/key/9c2db08baa7b78bef1927a3c7342fffb/')
       .done(function (data) {
         let amount = parseFloat(/.*;el.innerHTML="(\d+\.\d+)".*/g.exec(data)[1])
+        let amount_bank = 2500
         let full_amount = 5000
-        let percentage = amount / full_amount
+        let percentage = (amount + amount_bank) / full_amount
         $('#mercury').height(percentage * 500)
         $('#mercury').prop('title', amount + '€')
       })
