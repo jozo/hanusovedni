3e5a5c96090b5f29570c07dcdbf7859325197c8f
jozo <hi@jozo.io>
2020-04-18 16:20:41 +0200

    #68 - Add language prefix to urls in menu to fix problem CloudFlare caching redirection

--- src/home/templatetags/i18n_extra.py
+++ src/home/templatetags/i18n_extra.py
@@ -7,3 +7,14 @@ register = template.Library()
 @register.simple_tag
 def current_locale():
     return to_locale(get_language())
+
+
+@register.filter
+def localize_url(value):
+    if value and not value.startswith("http"):
+        locale = to_locale(get_language())
+        if value.startswith("/"):
+            return f"/{locale}{value}"
+        else:
+            return f"/{locale}/{value}"
+    return value
