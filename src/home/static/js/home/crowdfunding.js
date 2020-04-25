a97c55942b66d24f06f226da3889acfeeb65640b
jozo <hi@jozo.io>
2020-04-25 19:17:14 +0200

    #51 - Fix scale on thermometer

--- src/home/static/js/home/crowdfunding.js
+++ src/home/static/js/home/crowdfunding.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $.ajax('https://old.darujme.sk/sk/campaign/feed/2393/key/9c2db08baa7b78bef1927a3c7342fffb/')
       .done(function (data) {
         let amount = parseFloat(/.*;el.innerHTML="(\d+\.\d+)".*/g.exec(data)[1])
-        let amount_bank = 2500
+        let amount_bank = 1885
         let full_amount = 10000
         let display_amount = Math.min(amount + amount_bank, full_amount)
         let percentage = display_amount / full_amount
