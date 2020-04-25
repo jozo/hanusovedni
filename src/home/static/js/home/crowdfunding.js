f68048ae17fb3a8b33793ec5bb52fbd0056177f2
jozo <hi@jozo.io>
2020-04-25 16:46:58 +0200

    #51 - Fix scale on thermometer

--- src/home/static/js/home/crowdfunding.js
+++ src/home/static/js/home/crowdfunding.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
       .done(function (data) {
         let amount = parseFloat(/.*;el.innerHTML="(\d+\.\d+)".*/g.exec(data)[1])
         let amount_bank = 2500
-        let full_amount = 5000
+        let full_amount = 10000
         let display_amount = Math.min(amount + amount_bank, full_amount)
         let percentage = display_amount / full_amount
         $('#mercury').height(percentage * 500)
