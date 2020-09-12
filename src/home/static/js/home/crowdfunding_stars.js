f6546a92d3f7088af279aa5956c4cd3295c7604a
jozo <hi@jozo.io>
2020-09-12 09:23:48 +0200

    #83 - Add amount if http request fails

--- src/home/static/js/home/crowdfunding_stars.js
+++ src/home/static/js/home/crowdfunding_stars.js
@@ -1,21 +1,26 @@
 $(document).ready(function () {
+  function showPercentage(amount) {
+    let amount_bank = 0
+    let full_amount = 10000
+    let display_amount = Math.min(amount + amount_bank, full_amount)
+    let percentage = display_amount / full_amount
+    $('#mercury').height(percentage * 500)
+    $('#mercury').prop('title', (amount + amount_bank) + '€')
+
+    // Set size of stars backgrounds
+    $('#grey-overlay-1').height((1 - percentage) * 500 + "px")
+    $('#grey-overlay-1').css('bottom', percentage * 500 + "px")
+    $('#grey-overlay-2').height((1 - percentage) * 500 + "px")
+    $('#grey-overlay-2').css('bottom', percentage * 500 + "px")
+  }
+
     $.ajax('https://old.darujme.sk/sk/campaign/feed/2785/key/8277e0e414c8b5089c657c6bedd182e5/')
       .done(function (data) {
         let amount = parseFloat(/.*;el.innerHTML="(\d+\.\d+)".*/g.exec(data)[1])
-        let amount_bank = 0
-        let full_amount = 10000
-        let display_amount = Math.min(amount + amount_bank, full_amount)
-        let percentage = display_amount / full_amount
-        $('#mercury').height(percentage * 500)
-        $('#mercury').prop('title', (amount + amount_bank) + '€')
-
-        // Set size of stars backgrounds
-        $('#grey-overlay-1').height((1 - percentage) * 500 + "px")
-        $('#grey-overlay-1').css('bottom', percentage * 500 + "px")
-        $('#grey-overlay-2').height((1 - percentage) * 500 + "px")
-        $('#grey-overlay-2').css('bottom', percentage * 500 + "px")
+        showPercentage(amount)
       })
       .fail(function () {
         console.log('Error - can\'t get current amount of €')
+        showPercentage(60)
       })
   })
