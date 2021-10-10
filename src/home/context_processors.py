aed3686eee788aac3a6957cd52de67a41badb23f
jozo <hi@jozo.io>
2021-10-10 09:40:24 +0200

    #107 - Fix festival finder

--- src/home/context_processors.py
+++ src/home/context_processors.py
@@ -3,7 +3,8 @@ from home.models import FestivalPage
 
 def festival_finder(request):
     festival_slugs = set(FestivalPage.objects.values_list("slug", flat=True))
-    first_url_part = request.path.split("/")[2]
+    url_parts = request.path.split("/")
+    first_url_part = url_parts[2] if len(url_parts) > 2 else ""
     if first_url_part in festival_slugs:
         festival = FestivalPage.objects.get(slug=first_url_part)
     else:
